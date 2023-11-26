a41eb3ef8c4c1d2d15d4f936284fa12d80141957
Surya Murugan <surya3542live@gmail.com>
2023-11-26 12:28:06 +0530

    added routes

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,13 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+
+    { id: 2, name: locale.NAV.EVENTS, to: '/events', show: true },
+    { id: 3, name: locale.NAV.TEAM, to: '/team', show: true },
+    { id: 4, name: locale.NAV.POLICY, to: '/policy', show: true },
+
+    { id: 5, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
+    { id: 6, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
